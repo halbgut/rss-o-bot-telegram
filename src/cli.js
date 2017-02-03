eb69a4e7ee4b6891ca8d4f51a91633ad18af9ef7
Luca Nils Schmid <me@lucaschmid.net>
2017-02-03 19:16:43 +0100

    refactor(*): improve cli

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process')
-const Tg = require('tg-yarl')
-const Rx = require('rxjs/Rx')
-const O = Rx.Observable
-const rssOBot = require('rss-o-bot')
+const { Observable: O } = require('rxjs')
+
+const { sendMessage, instantiate, poll, runCommand } = require('./lib')
 
 const help =
 `rss-o-bot-telegram poll
@@ -19,44 +17,11 @@ const help =
 
 const action = process.argv[2]
 
-const instantiate = () =>
-  rssOBot.getConfig()
-    .map(config => [config, Tg(config.get('telegram-api-token'))])
-
-const poll = (filter = false) => ([config, tg]) =>
-  O.interval(2000)
-    .startWith(0)
-    .switchMap(() => O.fromPromise(tg.getUpdates(-1)))
-    .switchMap(res => res.body.ok
-      ? O.of(res.body.result)
-      : O.throw([res.status, res.body])
-    )
-    .concatMap(v => O.from(v))
-    .filter(v =>
-      !filter ||
-      // If `filter` is true, check that the sender is in `telegram-commanders`
-      config
-        .get('telegram-commanders')
-        .includes(v.message.from.id.toString())
-    )
-    .distinctUntilChanged((a, b) => b.update_id === a.update_id)
-    .skip(1)
-
-const runCommand = args => O.create(o => {
-  const proc = spawn('rss-o-bot', args)
-  let data = ''
-  proc.stdout.on('data', x => { data += x })
-  proc.stdout.on('error', x => { data += x })
-  proc.on('close', () => {
-    o.next(data)
-  })
-})
-
 if (action === 'poll') {
   instantiate()
     .switchMap(poll())
-    .map(update => update ? update.message.from.id : null)
-    .filter(a => a)
+    .filter(update => update)
+    .map(update => update.message.from.id)
     .subscribe(console.log, console.error)
 } else if (action === 'run') {
   instantiate()
@@ -64,12 +29,12 @@ if (action === 'poll') {
       poll(true)([config, tg])
         .concatMap(t =>
           runCommand(t.message.text.split(' '))
-            .switchMap(data => data.split('\n'))
-            .concatMap(msg => O.fromPromise(tg.sendMessage(t.message.chat.id, msg)))
+            .concatMap(v => O.from(v.split(1000)))
+            .concatMap(sendMessage(tg, t))
         )
     )
+    .last()
     .subscribe(console.log, console.error)
 } else {
   console.log(help)
 }
-
---
+++ src/lib/index.js
@@ -0,0 +1,45 @@
+const { spawn } = require('child_process')
+const { Observable: O } = require('rxjs')
+const rssOBot = require('rss-o-bot')
+const Tg = require('tg-yarl')
+
+module.exports.sendMessage = (tg, t) => msg => O.fromPromise(
+  tg.sendMessage(
+    t.message.chat.id,
+    msg,
+    { parse_mode: 'HTML' }
+  )
+)
+
+module.exports.instantiate = () =>
+  rssOBot.getConfig()
+    .map(config => [config, Tg(config.get('telegram-api-token'))])
+
+module.exports.poll = (filter = false) => ([config, tg]) =>
+  O.interval(2000)
+    .startWith(0)
+    .switchMap(() => O.fromPromise(tg.getUpdates(-1)))
+    .switchMap(res => res.body.ok
+      ? O.of(res.body.result)
+      : O.throw([res.status, res.body])
+    )
+    .concatMap(v => O.from(v))
+    .filter(v =>
+      !filter ||
+      // If `filter` is true, check that the sender is in `telegram-commanders`
+      config
+        .get('telegram-commanders')
+        .includes(v.message.from.id.toString())
+    )
+    .distinctUntilChanged((a, b) => b.update_id === a.update_id)
+    .skip(1)
+
+module.exports.runCommand = args => O.create(o => {
+  const proc = spawn('rss-o-bot', args)
+  let data = ''
+  proc.stdout.on('data', x => { data += x })
+  proc.stdout.on('error', x => { data += x })
+  proc.on('close', () => {
+    o.next(data)
+  })
+})
